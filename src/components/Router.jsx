4f4a67ed50750c234515624d387de12d58a31aff
Faisal Bahari <fbhr1112@gmail.com>
2023-12-23 21:10:45 +0700

    fix(route)

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import Ideas from '../pages/Ideas'
 import Blank from '../pages/Blank'
 
 export default function Router() {
     return (
-        <Routes>
-            <Route path='/ideas' element={<Ideas />} />
-            <Route path='/' element={<Blank />} />
-            <Route path='*' element={<Blank />} />
-        </Routes>
+        <BrowserRouter>
+            <Routes>
+                <Route path='/ideas' element={<Ideas />}></Route>
+                <Route path='/' element={<Blank />}></Route>
+                <Route path='*' element={<Blank />}></Route>
+            </Routes>
+        </BrowserRouter>
     )
 }
