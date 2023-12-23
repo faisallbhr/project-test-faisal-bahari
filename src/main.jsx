8c30922529a75492efd4e526eba114ba51df334f
Faisal Bahari <fbhr1112@gmail.com>
2023-12-23 21:00:02 +0700

    delete react-query

--- src/main.jsx
+++ src/main.jsx
@@ -2,24 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { QueryClient, QueryClientProvider } from 'react-query';
 import { BrowserRouter } from 'react-router-dom';
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false, // Atur ke false agar query tidak secara otomatis mereload saat fokus ke window
-      retry: 3, // Jumlah retry ketika terjadi kesalahan
-    },
-  },
-});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </QueryClientProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
